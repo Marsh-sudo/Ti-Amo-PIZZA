1a90b89e86fd4dc16aa00141249595b4cac1e555
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-01-17 01:35:11 +0300

    Add case/switch to js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let total = 0;
+
 
 function Pizza(flavour,crust,size,toppings,total) {
     this.flavour = flavour;
@@ -10,28 +10,30 @@ function Pizza(flavour,crust,size,toppings,total) {
 
 // user interface
 $(document).ready(function() {
-    $("form#pizza-option").submit(function(event){
+    $("button.Continue").click(function(event){
         event.preventDefault();
 
         var inputtedFlavour = $("#flavour option:selected").val();
         var inputtedSize = $("#size option:selected").val();
         var inputtedCrust = $("#crust option:selected").val();
+        var inputtedToppings = $("input#top").val();
+  
+        inputValues(inputtedCrust,inputtedFlavour,inputtedSize,inputtedToppings)
 
-
-    })
-    let inputtedSize;
+    });
+    
     switch(inputtedSize){
         case 'small':
-          cost = 850;
+          siCost = 850;
         break;
         case 'medium':
-          cost = 950;
+          siCost = 950;
         break;
         case 'large':
-           cost = 1050; 
+           siCost = 1050; 
         break;
         case 'x-large':
-            cost =  1500;  
+            siCost =  1500;  
     }
     let inputtedCrust;
     switch(inputtedCrust) {
@@ -45,4 +47,30 @@ $(document).ready(function() {
             CrCost = 300;
             break;        
     }
-})
+    let inputtedToppings;
+    switch(inputtedToppings){
+        case 'bacon':
+            toCost = 200;
+            break;
+        case 'sausage':
+            toCost = 150;
+            break;
+        case 'onions':
+            toCost = 50;
+            break;
+        case 'peppers':
+            toCost = 50;           
+    }
+
+    // cost of everything
+    let norm = 0;
+    let totalCost = 0
+    norm = toCost + siCost + CrCost;
+    totalCost = norm + totalCost;
+     
+    $("#Orders").html(norm);
+   
+
+
+
+});
