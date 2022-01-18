42893972df06bc70bf6816d168542200a1c63342
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-01-19 01:37:15 +0300

    change onclick statement

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-
+let total = 0;
 function Pizza(flavour,crust,size,toppings,total) {
     this.flavour = flavour;
     this.crust = crust;
@@ -13,64 +13,74 @@ $(document).ready(function() {
     $("button.Continue").click(function(event){
         event.preventDefault();
 
-        var inputtedFlavour = $("#flavour option:selected").val();
-        var inputtedSize = $("#size option:selected").val();
-        var inputtedCrust = $("#crust option:selected").val();
-        var inputtedToppings = $("input#top").val();
+        let inputtedFlavour = $("#flavour option:selected").val();
+        let inputtedSize = $("#size option:selected").val();
+        let inputtedCrust = $("#crust option:selected").val();
+        let inputtedToppings = $("input#top").val();
   
-        inputValues(inputtedCrust,inputtedFlavour,inputtedSize,inputtedToppings)
+       let siCost;
+       let CrCost;
+       let toCost;
+
+        switch(inputtedSize){
+            case 'small':
+              siCost = 850;
+            break;
+            case 'medium':
+              siCost = 950;
+            break;
+            case 'large':
+               siCost = 1050; 
+            break;
+            case 'x-large':
+                siCost =  1500;  
+        }
+        
+        switch(inputtedCrust) {
+            case 'Gluten':
+                CrCost = 150;
+                break;
+            case 'crispy':
+                CrCost = 200;
+                break;
+            case 'stuffed':
+                CrCost = 300;
+                break;        
+        }
+        
+        switch(inputtedToppings){
+            case 'bacon':
+                toCost = 200;
+                break;
+            case 'sausage':
+                toCost = 150;
+                break;
+            case 'onions':
+                toCost = 50;
+                break;
+            case 'peppers':
+                toCost = 50;           
+        }
+    
+        // cost of everything
+        total = siCost + CrCost + toCost;
+        let totalCost = 0;
+        totalCost = totalCost + total;
+         
+        $("div.finalOutput").slideDown();
+        
+        $("#pflavour").html($("#flavour option:selected").val());
+        $("#psize").html($("#size option:selected").val());
+        $("#pcrust").html($("#crust option:selected").val());
+        $("#pcost").html(total);
+        console.log(total);
+        
+       
 
     });
+
+    $("button#AnotherPizza").click(function(){
+        
+    })
     
-    switch(inputtedSize){
-        case 'small':
-          siCost = 850;
-        break;
-        case 'medium':
-          siCost = 950;
-        break;
-        case 'large':
-           siCost = 1050; 
-        break;
-        case 'x-large':
-            siCost =  1500;  
-    }
-    let inputtedCrust;
-    switch(inputtedCrust) {
-        case 'Gluten':
-            CrCost = 150;
-            break;
-        case 'crispy':
-            CrCost = 200;
-            break;
-        case 'stuffed':
-            CrCost = 300;
-            break;        
-    }
-    let inputtedToppings;
-    switch(inputtedToppings){
-        case 'bacon':
-            toCost = 200;
-            break;
-        case 'sausage':
-            toCost = 150;
-            break;
-        case 'onions':
-            toCost = 50;
-            break;
-        case 'peppers':
-            toCost = 50;           
-    }
-
-    // cost of everything
-    let norm = 0;
-    let totalCost = 0
-    norm = toCost + siCost + CrCost;
-    totalCost = norm + totalCost;
-     
-    $("#Orders").html(norm);
-   
-
-
-
 });
