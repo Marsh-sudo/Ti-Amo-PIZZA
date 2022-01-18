9f82a4b8d2190d93c238ac9c88d20a271a7fa0d1
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-01-19 02:36:15 +0300

    Append statement to html using jquery

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-
+let name;
+let size;
+let crust;
 let total = 0;
 function Pizza(flavour,crust,size,toppings,total) {
     this.flavour = flavour;
@@ -10,7 +12,7 @@ function Pizza(flavour,crust,size,toppings,total) {
 
 // user interface
 $(document).ready(function() {
-    $("button.Continue").click(function(event){
+    $(".Continue").click(function(event){
         event.preventDefault();
 
         let inputtedFlavour = $("#flavour option:selected").val();
@@ -79,8 +81,48 @@ $(document).ready(function() {
 
     });
 
-    $("button#AnotherPizza").click(function(){
+    $("#AnotherPizza").click(function(){
+        let pizzaName = $("#flavour option:selected").val();
+        let pizzaSize = $("#size option:selected").val();
+        let pizzaCrust = $("#crust option:selected").val();
+
+        switch(pizzaSize){
+            case 'small':
+              siCost = 850;
+            break;
+            case 'medium':
+              siCost = 950;
+            break;
+            case 'large':
+               siCost = 1050; 
+            break;
+            case 'x-large':
+                siCost =  1500;  
+        }
         
+        switch(pizzaCrust) {
+            case 'Gluten':
+                CrCost = 150;
+                break;
+            case 'crispy':
+                CrCost = 200;
+                break;
+            case 'stuffed':
+                CrCost = 300;
+                break;        
+        }
+        total = siCost + CrCost;
+        totalCost = totalCost + total;
+     
+    })
+
+    $("#checkout").click(function(){
+        $("pizzacost").append("Your cost is" + " " + totalCost);
     })
     
+    $("#Order2").click(function(){
+        let deliveryCost = totalCost + 150;
+        $("#totalbills").append("Your total cost plus delivery fee" + " " + deliveryCost);
+    })
+
 });
